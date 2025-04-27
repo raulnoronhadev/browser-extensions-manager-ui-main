e169fe17e35a1e49f28bfaee5e26cba32e327fab
raulnoronhadev <seuemail@gmail.com>
2025-04-27 13:01:07 -0300

    Extensions switch configuration.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-$(document).ready(function() {
-    $.getJSON('data.json', function(data) {
-        console.log(data);
+let data = []
 
-        data.forEach(function(item) {
+$(document).ready(function() {
+    $.getJSON('data.json', function(response) {
+        data = response;
+        console.log(data);
+        data.forEach(function(item, index) {
             $('.container-grid-extensions').append(`
                 <div class="extension-box">
                     <div class="img_and_content">
@@ -14,10 +16,33 @@ $(document).ready(function() {
                     </div>
                     <div class="extension-options">
                             <button class="remove">Remove</button>
-                            
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" 
+                                    id="mySwitch-${index}" 
+                                    name="darkmode" 
+                                    value="yes"
+                                    ${item.isActive ? 'checked' : ''}
+                                    data-index="${index}"
+                                >
+                            </div>
                     </div>
                 </div>
             `)
         })
     })
+})
+
+let filterOption = $(".filters ul > li");
+
+filterOption.on("click", function() {
+    filterOption.removeAttr('id');
+    $(this).attr('id', 'active');
+})
+
+$(".container-grid-extensions").on("change", ".form-check-input", function() {
+    let index = $(this).data('index');
+    let isChecked = $(this).is(":checked");
+    data[index].isActive = isChecked;
+    console.log(`Switch do item ${index} alterado para: ${isChecked}`);
+    console.log(data);
 })
