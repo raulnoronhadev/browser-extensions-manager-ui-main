175735c9efb3e5929009753347d80147d274a938
raulnoronhadev <seuemail@gmail.com>
2025-04-27 16:29:10 -0300

    Configure remove extension

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let data = []
 $(document).ready(function() {
     $.getJSON('data.json', function(response) {
         data = response;
-        console.log(data);
         data.forEach(function(item, index) {
             $('.container-grid-extensions').append(`
                 <div class="extension-box">
@@ -15,7 +14,7 @@ $(document).ready(function() {
                         </div>
                     </div>
                     <div class="extension-options">
-                            <button class="remove">Remove</button>
+                            <button class="remove-button">Remove</button>
                             <div class="form-check form-switch">
                                 <input class="form-check-input" type="checkbox" 
                                     id="mySwitch-${index}" 
@@ -32,7 +31,13 @@ $(document).ready(function() {
     })
 })
 
+// filter options
+
 let filterOption = $(".filters ul > li");
+let allOption = $(".all-button");
+let activeOption = $(".active-button");
+let inactiveOption = $(".inactive-button");
+let removeButton = $(".remove-button");
 
 filterOption.on("click", function() {
     filterOption.removeAttr('id');
@@ -43,6 +48,42 @@ $(".container-grid-extensions").on("change", ".form-check-input", function() {
     let index = $(this).data('index');
     let isChecked = $(this).is(":checked");
     data[index].isActive = isChecked;
-    console.log(`Switch do item ${index} alterado para: ${isChecked}`);
-    console.log(data);
 })
+
+allOption.on("click", function() {
+    data.forEach(function(item, index) {
+        let extensionBox = $("#mySwitch-" + index).closest(".extension-box");
+        extensionBox.show();
+    })
+})
+
+activeOption.on("click", function() {
+    data.forEach(function(item, index) {
+        let extensionBox = $("#mySwitch-" + index).closest(".extension-box");
+        if (item.isActive == false) {
+            extensionBox.hide();
+        } else {
+            extensionBox.show();
+        }  
+    })
+})
+
+inactiveOption.on("click", function() {
+    data.forEach(function(item, index) {
+        let extensionBox = $("#mySwitch-" + index).closest(".extension-box");
+        if (item.isActive == true) {
+            extensionBox.hide();
+        } else {
+            extensionBox.show();
+        }  
+    })
+})
+
+$(".container-grid-extensions").on("click", ".remove-button", function() {
+    let extensionBox = $(this).closest(".extension-box");
+    extensionBox.hide();
+})
+
+// modify theme
+
+
